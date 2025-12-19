19dfe61231220460d16e68cebaae67d71bf12522
Conrad <rifeconrad@gmail.com>
2025-12-19 12:39:31 -0800

    vector transformation with matrix 2d

--- Basics/vector_math.py
+++ Basics/vector_math.py
@@ -1,9 +1,16 @@
-class CustomVector:
+class Matrix2d:
+    def __init__(self, rows):
+        self.rows = rows
+
+class Vector:
     def __init__(self, coordinates, name=""):
         # Only accept 2d, 3d, or 4d vectors
         if (len(coordinates) > 4 or len(coordinates) < 2):
             raise Exception("Only 2d, 3d, or 4d vectors are accepted.")
         self.coordinates = coordinates
+        self.name = name
+
+    def renameVector(self, name):
         self.name = name
 
     # Function for visual convenience
@@ -34,53 +41,32 @@ class CustomVector:
             self.coordinates[i] = self.coordinates[i] * scalar
 
 
-class Vector2d:
+class Vector2d(Vector):
     def __init__(self, x, y, name=""):
-        self.x = x
-        self.y = y
-        self.name = name
+        super().__init__([x, y], name)
 
-    # Function for visual convenience
-    def printCoordinates(self):
-        print(self.name, ": ", self.x, ", ", self.y)
-    
-    # Vectors added together are the sum of their parts x1+x2, y1+y2, etc.
-    def addVector(self, vector):
-        print("Adding vectors: ", self.coordinates, " + ", vector.coordinates)
-        self.x = self.x + vector.x
-        self.y = self.y + vector.y
+    def transformVector(self, matrix):
+        a = self.coordinates[0] * matrix.rows[0]
+        b = self.coordinates[1] * matrix.rows[1]
+        c = self.coordinates[0] * matrix.rows[2]
+        d = self.coordinates[1] * matrix.rows[3]
+        self.coordinates[0] = a+b
+        self.coordinates[1] = c+d
 
-    # Vector scaling is to multiply each coordinate by the scalar
-    # - Scalar of >1 increases the size of the vector
-    # - Scalar of 1 maintains the vector's size
-    # - Scalar of >= 0 AND < 1 reduces the size of the vector
-    # - Scalar of < 0 flips the vector while maintaining the origin
-    def scaleVector(self, scalar):
-        print("Scaling vector by ", scalar)
-        self.x = self.x * scalar
-        self.y = self.y * scalar
+class Vector3d(Vector):
+    def __init__(self, x, y, z, name=""):
+        super().__init__([x, y, z], name)
 
-def Vector2d_Test():
-    v1 = Vector2d(1, 2, "v1")
-    v2 = Vector2d(3, 3, "v2")
-    v1.printCoordinates()
-    v2.printCoordinates()
-    v1.addVector(v2)
-    v1.printCoordinates()
-    v2.printCoordinates()
-    v2.scaleVector(2)
-    v1.printCoordinates()
-    v2.printCoordinates()
-    v1.scaleVector(-1)
-    v1.printCoordinates()
-    v2.printCoordinates()
+class Vector4d(Vector):
+    def __init__(self, w, x, y, z, name=""):
+        super().__init__([w, x, y, z], name)
 
 def CustomVector_Test():
-    v1 = CustomVector([1, 2], "v1")
+    v1 = Vector([1, 2], name="v1")
     v1.printCoordinates()
-    v2 = CustomVector([4, 5], "v2")
+    v2 = Vector([4, 5], name="v2")
     v2.printCoordinates()
-    v3 = CustomVector([1, 2, 4, 5], "v3")
+    v3 = Vector([1, 2, 4, 5], name="v3")
     v3.printCoordinates()
     v2.addVector(v1)
     v2.printCoordinates()
@@ -93,4 +79,34 @@ def CustomVector_Test():
     v3.scaleVector(3)
     v3.printCoordinates()
 
-CustomVector_Test()
+def Vector2d_Test():
+    v1 = Vector2d(1, 2, name="v1")
+    v2 = Vector2d(3, 3, name="v2")
+    v1.printCoordinates()
+    v2.printCoordinates()
+    v1.addVector(v2)
+    v1.printCoordinates()
+    v2.printCoordinates()
+    v2.scaleVector(2)
+    v1.printCoordinates()
+    v2.printCoordinates()
+    v1.scaleVector(-1)
+    v1.printCoordinates()
+    v2.printCoordinates()
+
+def Vector_ChildClasses_Test():
+    v2d = Vector2d(1, 2, name="v2d")
+    v4d = Vector4d(3, 4, 5, 6, name="v4d")
+    v2d.printCoordinates()
+    v2d.addVector(v4d)
+    v2d.printCoordinates()
+
+def Vector2d_Rotate_90def():
+    v2d = Vector2d(2, 1, name="vector")
+    v2d.printCoordinates()
+    matrix = Matrix2d([0, -1, 1, 0])
+    v2d.transformVector(matrix)
+    v2d.renameVector("rotated-vector")
+    v2d.printCoordinates()
+
+Vector2d_Rotate_90def()
